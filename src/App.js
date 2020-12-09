fe45d97d72936874dc135102ff5c7b1dde2a6072
tsamantanis <philip.tsamantanis@icloud.com>
2020-12-09 15:59:07 -0800

    Add functioning search (Stretch challenge)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import Heading from './components/Heading';
 import MetalMeta from './components/MetalMeta';
-import Band from './components/Band';
-
-import data from './metal.json';
+import Bands from './components/Bands';
 
 function App() {
     return (
         <div className="container">
             <Heading />
             <MetalMeta />
-            <div className="row mt-5">
-                { data.map((band) => {
-                    return (
-                        <Band
-                            key={ band.band_name }
-                            name={ band.band_name }
-                            formed={ band.formed }
-                            origin={ band.origin }
-                            fans={ band.fans }
-                            split={ band.split }
-                            // style={ band.style }
-                        />
-                    );
-                })}
-            </div>
+            <Bands />
         </div>
     );
 }
---
+++ src/components/Bands.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import Band from './Band';
+import data from '../metal.json';
+
+function Bands() {
+    const [search, setSearch] = useState('')
+
+    let filteredData = data.filter((band) => {
+        const searchable = band.band_name + ' ' + band.formed + ' '
+            + band.origin + ' ' + band.fans + ' ' + band.style;
+        const searchTerms = search.toLowerCase().trim().split(' ');
+        return searchTerms.every(term => {
+            return searchable.toLowerCase().includes(term);
+        });
+    })
+    return (
+        <>
+            <div className="row mt-5 justify-content-center align-items-center">
+                <div className="col-12 col-md-4">
+                    <input
+                        name="search"
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                        placeholder="Search for a band"
+                    />
+                </div>
+            </div>
+            <div className="row mt-5">
+                { filteredData.map((band) => {
+                    return (
+                        <Band
+                            key={ band.band_name }
+                            name={ band.band_name }
+                            formed={ band.formed }
+                            origin={ band.origin }
+                            fans={ band.fans }
+                            split={ band.split }
+                            // style={ band.style }
+                        />
+                    );
+                })}
+            </div>
+        </>
+    );
+}
+
+export default Bands;
