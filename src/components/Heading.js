786a040b2c2d908194d3aa3c90b8e21992777016
tsamantanis <philip.tsamantanis@icloud.com>
2020-12-09 15:02:34 -0800

    Heading component

---
+++ src/components/Heading.js
@@ -0,0 +1,9 @@
+function Heading() {
+    return (
+        <div>
+            <h1>Final Assessment</h1>
+            <h2>Philippos Tsamantanis</h2>
+        </div>
+    );
+}
+export default Heading
